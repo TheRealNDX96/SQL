59634b9a29e2e719f9fcbd06e969c51aee98683c
TheRealNDX96 <84760129+TheRealNDX96@users.noreply.github.com>
2021-08-11 10:37:25 +0800

    Create country_vaccination2.sql

---
+++ COVID19Project/country_vaccination2.sql
@@ -0,0 +1,15 @@
+# Data Source: https://www.kaggle.com/gpreda/covid-world-vaccination-progress?select=country_vaccinations.csv
+
+--- Original Data
+SELECT *
+FROM COVID19.dbo.country_vacc
+
+--- People vaccinated per hundred based on latest available date for each country
+SELECT co.country, total_vaccinations, total_vaccinations_per_hundred, people_vaccinated_per_hundred
+FROM COVID19.dbo.country_vacc co
+INNER JOIN
+(SELECT country, max(date) AS max_date
+FROM COVID19.dbo.country_vacc
+GROUP BY country 
+) md
+ON md.max_date = co.date AND md.country = co.country
